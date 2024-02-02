3a3dca09c3300185f403eae3fecbcadf4c9de685
Tristan Alonso <tristanalonso09@gmail.com>
2024-02-02 13:20:52 +0100

    roles and roles_user tables Reworked

--- database/migrations/2024_02_01_192743_create_roles_table.php
+++ database/migrations/2024_02_01_192743_create_roles_table.php
@@ -13,7 +13,7 @@ return new class extends Migration
     {
         Schema::create('roles', function (Blueprint $table) {
             $table->id();
-            $table->string('rolname');
+            $table->string('rolename');
             $table->timestamps();
         });
     }
--- database/migrations/2024_02_01_193118_create_role_user_table.php
+++ database/migrations/2024_02_01_193118_create_role_user_table.php
@@ -11,9 +11,9 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('user_roles', function (Blueprint $table) {
+        Schema::create('role_user', function (Blueprint $table) {
             $table->id();
-            $table->foreignId('rol_id')->constrained(table: 'roles')->cascadeOnUpdate()->cascadeOnDelete();
+            $table->foreignId('role_id')->constrained()->cascadeOnUpdate()->cascadeOnDelete();
             $table->foreignId('user_id')->constrained()->cascadeOnUpdate()->cascadeOnDelete();
             $table->timestamps();
         });
@@ -24,6 +24,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('user_roles');
+        Schema::dropIfExists('role_user');
     }
 };
